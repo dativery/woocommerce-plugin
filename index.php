41eea5c430bd88025d6afa8214c5a33b39aca672
JosefPadua <68616810+JosefPadua@users.noreply.github.com>
2023-08-27 22:44:35 +0200

    Opravení podpory number a přejmenování pluginu

--- index.php
+++ index.php
@@ -1,32 +1,29 @@
 <?php
 /**
- * @package Dativery: modified_after support
- * @version 1.0
+ * @package Dativery: modified_after and number support
+ * @version 1.1
  */
 /*
-Plugin Name: Dativery: modified_after support
-Version: 1.0
+Plugin Name: Dativery: modified_after and number support
+Version: 1.1
 */
 
 add_filter('woocommerce_rest_orders_prepare_object_query', function(array $args, \WP_REST_Request $request) {
 	$modified_after = $request->get_param('modified_after');
+	$number = $request->get_param('number');
 
-	if ($modifed_after) {
+	if ($modified_after) {
 	    $args['date_query'][0]['column'] = 'post_modified';
 	    $args['date_query'][0]['after']  = $modified_after;
 	    $args['orderby'] = 'post_modified';
 	    $args['order'] = 'asc';
 	}
 
-	$number = $request->get_param('number');
-
 	if ($number) {
-	    $args['s'] = $number;
+		$args['post__in'] = array( intval( $number ) );
 	}
-
 
     return $args;
 
 }, 10, 2);
-
 ?>
